a6fd5f9c9c47764003cbdb6a31f4190b358a0c54
M.Zayniddin <clon3.secur1ty@gmail.com>
2024-12-20 01:08:51 +0500

    feat: added score

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,12 @@
-using UnityEngine.InputSystem;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
+
+    private int score = 0;
 
     private float movementX;
     private float movementY;
@@ -11,9 +14,14 @@ public class PlayerController : MonoBehaviour
     // Speed at which the player moves.
     public float speed = 0;
 
+    public TextMeshProUGUI scoreText;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        score = 0;
+
+        SetScoreText();
     }
 
     void OnMove(InputValue movementValue)
@@ -24,9 +32,25 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
     }
 
+    void SetScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            score++;
+
+            SetScoreText();
+        }
     }
 }
